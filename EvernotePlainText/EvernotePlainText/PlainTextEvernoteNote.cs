01851716ec02dd1c35ca5f10cae0980eac9d013a
mdberrill <mdberrill@yahoo.co.uk>
2017-08-06 12:18:21 +0200

    GetNote, GetLatestAccountUpdateNo, & GetNotesChangedSinceUpdateNo

---
+++ EvernotePlainText/EvernotePlainText/PlainTextEvernoteNote.cs
@@ -0,0 +1,16 @@
+﻿namespace EvernotePlainText
+{
+    public class PlainTextEvernoteNote
+    {
+        public string PlainTextContext { get; private set; }
+        public string Title { get; private set; }
+        public int UpdateNo { get; private set; }
+
+        public PlainTextEvernoteNote(string plainText, string title, int updateNo)
+        {
+            PlainTextContext = plainText;
+            Title = title;
+            UpdateNo = updateNo;
+        }
+    }
+}
